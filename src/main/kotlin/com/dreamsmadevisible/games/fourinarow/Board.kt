6b41ffd9696ebf4bae9f99d7059e56cac2c5c5d7
Matthew Jacobs <5423966+mattj256@users.noreply.github.com>
2020-11-23 21:41:15 -0500

    change internal board representation from rows to cols

--- src/main/kotlin/com/dreamsmadevisible/games/fourinarow/Board.kt
+++ src/main/kotlin/com/dreamsmadevisible/games/fourinarow/Board.kt
@@ -4,9 +4,23 @@ class Board() {
     val boardString : String
 
     init {
-        val row = "-".repeat(BOARD_WIDTH)
-        boardString = row + ("/" + row).repeat(BOARD_HEIGHT - 1)
+        val col = "-".repeat(BOARD_HEIGHT)
+        boardString = col + ("/" + col).repeat(BOARD_WIDTH - 1)
     }
 
     fun getDebugBoardString() : String = boardString
+
+    /*
+    fun move(col: Int, player: Player) : Board {
+        boardString.su
+
+        return this
+    }
+
+    private fun getColString(col: Int): String =
+            boardString
+                    .split("/")
+                    .map { x -> x.get(col)}
+                    .toString()
+     */
 }
