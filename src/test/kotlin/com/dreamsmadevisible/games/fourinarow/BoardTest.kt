9608abc6a98d627b5ff1c3a62909d9c236b86e4b
Matthew Jacobs <5423966+mattj256@users.noreply.github.com>
2020-11-23 21:15:41 -0500

    add dummy test file

---
+++ src/test/kotlin/com/dreamsmadevisible/games/fourinarow/BoardTest.kt
@@ -0,0 +1,13 @@
+package com.dreamsmadevisible.games.fourinarow
+
+import kotlin.test.assertEquals
+import org.junit.Test
+import kotlin.test.assertTrue
+
+class BoardTest {
+    // TODO: remove
+    @Test
+    fun foo() {
+        assertTrue(true)
+    }
+}
