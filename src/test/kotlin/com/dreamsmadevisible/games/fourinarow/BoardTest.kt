5a937851820b4110543c8a4a8face8fe3f2997bf
Matthew Jacobs <5423966+mattj256@users.noreply.github.com>
2020-11-23 22:52:14 -0500

    add JUnitParamsRunner and multiple tests for Board.move()

--- src/test/kotlin/com/dreamsmadevisible/games/fourinarow/BoardTest.kt
+++ src/test/kotlin/com/dreamsmadevisible/games/fourinarow/BoardTest.kt
@@ -1,9 +1,13 @@
 package com.dreamsmadevisible.games.fourinarow
 
+import junitparams.JUnitParamsRunner
+import junitparams.Parameters
 import kotlin.test.assertEquals
 import org.junit.Test
+import org.junit.runner.RunWith
 import kotlin.test.assertTrue
 
+@RunWith(value = JUnitParamsRunner::class)
 class BoardTest {
 
     @Test
@@ -12,8 +16,25 @@ class BoardTest {
     }
 
     @Test
-    fun move() {
-        assertEquals("-----X/------/------/------/------/------/------", Board().move(0, Player.X).getDebugBoardString())
+    @Parameters(
+            "-----X/------/------/------/------/------/------|0|X",
+            "------/-----X/------/------/------/------/------|1|X",
+            "------/------/-----X/------/------/------/------|2|X",
+            "------/------/------/-----X/------/------/------|3|X",
+            "------/------/------/------/-----X/------/------|4|X",
+            "------/------/------/------/------/-----X/------|5|X",
+            "------/------/------/------/------/------/-----X|6|X",
+            "-----O/------/------/------/------/------/------|0|O",
+            "------/-----O/------/------/------/------/------|1|O",
+            "------/------/-----O/------/------/------/------|2|O",
+            "------/------/------/-----O/------/------/------|3|O",
+            "------/------/------/------/-----O/------/------|4|O",
+            "------/------/------/------/------/-----O/------|5|O",
+            "------/------/------/------/------/------/-----O|6|O"
+    )
+    fun move(expected: String, row: Int, player: Player) {
+        val board = Board().move(row, player)
+        assertEquals(expected, board.getDebugBoardString())
     }
 
 }
