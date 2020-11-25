bcede936eb90366b5141563cefc6c577d242cc3c
Matthew Jacobs <5423966+mattj256@users.noreply.github.com>
2020-11-24 19:56:46 -0500

    refactor Board so internal representation is immutable

--- src/main/kotlin/com/dreamsmadevisible/games/fourinarow/Board.kt
+++ src/main/kotlin/com/dreamsmadevisible/games/fourinarow/Board.kt
@@ -1,30 +1,33 @@
 package com.dreamsmadevisible.games.fourinarow
 
-class Board() {
-    private val EMPTY_SQUARE_CHAR = "-"
+class Board {
 
-    var boardString : String
+    val boardString : String
 
-    init {
+    companion object {
+        val EMPTY_SQUARE_CHAR = "-"
+        val COLUMN_DELIMITER = "/"
         val col = EMPTY_SQUARE_CHAR.repeat(BOARD_HEIGHT)
-        boardString = col + ("/" + col).repeat(BOARD_WIDTH - 1)
+        val emptyBoardString = col + ("/" + col).repeat(BOARD_WIDTH - 1)
     }
 
-    private constructor(_boardString: String): this() {
+    constructor(): this(emptyBoardString)
+
+    private constructor(_boardString: String) {
         boardString = _boardString
     }
 
     fun getDebugBoardString() : String = boardString
 
     fun move(row: Int, player: Player) : Board {
-        val cols = boardString.split("/").toMutableList()
+        val cols = boardString.split(COLUMN_DELIMITER).toMutableList()
         val oldCol = cols[row]
         val index = oldCol.lastIndexOf(EMPTY_SQUARE_CHAR)
         val stringBuilder = StringBuilder(oldCol)
         stringBuilder.setCharAt(index, player.char)
         val newCol = stringBuilder.toString()
         cols.set(row, newCol)
-        val newBoardString = cols.joinToString(separator = "/")
+        val newBoardString = cols.joinToString(separator = COLUMN_DELIMITER)
         return Board(newBoardString)
     }
 }
